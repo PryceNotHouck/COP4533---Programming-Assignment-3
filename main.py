da186ab962b280192c3319d21329d763ea3f011c
TDebord <t.debord15@gmail.com>
2026-04-06 22:17:45 -0400

    cleaned comments in main.py

--- main.py
+++ main.py
@@ -7,14 +7,8 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    # ex_characters = {'a': 2, 'b': 4, 'c': 5}
-    # ex_sequences = ['aacab', 'caab']
-
-
-    #k = number of letters, n = length of strings
-    #randomly selects letters to use, constructs two strings randomly of length n given the randomly selected k letters
-    # text = generate_input(5, 10)
     
+    #adjust input sizes here
     input_sizes = [25, 50, 75, 100, 125, 150, 175, 200, 225, 250]
     #generate 10 inputs of length n, dictated by the input_size list (10 total inputs/files)
     for i in range(1,11):
@@ -35,7 +29,6 @@ if __name__ == "__main__":
         end_t = time.perf_counter()
         runtimes.append(end_t-start_t)
         
-
     #graph runtimes
     plt.scatter(input_sizes, runtimes, color='red', label='Runtimes')
     z = np.polyfit(x=input_sizes, y=runtimes, deg=len(input_sizes))
@@ -47,26 +40,3 @@ if __name__ == "__main__":
     plt.legend()
     plt.grid(True)
     plt.show()
-    
-    # #converts input_generator text into dictionary, list values
-    # lines = text.strip().split("\n")
-    # print(lines)
-    # entries = lines[0]
-    # entry_vals = lines[1:int(entries)]
-    # print(entries)
-    # input_dict = {}
-    # sequence_list = []
-    # for line in entry_vals:
-    #     key, val = line.split()
-    #     input_dict[key] = int(val)
-    # sequence_list = lines[-2:]    
-    
-    # print(input_dict)
-    # print(sequence_list)
-
-    # print("MAX SUBSEQUENCE CALCULATIONS:")
-    
-    # value, sequence = max_subsequence.max_subsequence(input_dict, sequence_list)
-    # print(value)
-    # print(sequence)
-    
